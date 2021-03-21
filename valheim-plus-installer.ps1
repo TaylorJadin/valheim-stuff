122545d10df649ea28164a031566a2ba9c7dd0a5
Taylor Jadin <jadin.taylor@gmail.com>
2021-03-21 17:34:16 -0500

    more admin changes

--- valheim-plus-installer.ps1
+++ valheim-plus-installer.ps1
@@ -1,22 +1,3 @@
-# Run as admin
-param([switch]$Elevated)
-
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) {
-        # tried to elevate, did not work, aborting
-    } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-    }
-    exit
-}
-
-'running with full privileges'
-
 # Get-Folder Function
 
 Function Get-Folder($initialDirectory="")
