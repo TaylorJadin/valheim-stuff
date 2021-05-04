fdd78d6d001677483d7011e2d3ca4b344276d1a0
Taylor Jadin <jadin.taylor@gmail.com>
2021-05-03 19:31:14 -0500

    Update valheim-plus-installer.ps1

--- valheim-plus-installer.ps1
+++ valheim-plus-installer.ps1
@@ -55,7 +55,7 @@ else {
 
 # Download latest release from github
 $releases = "https://api.github.com/repos/$repo/releases"
-Write-Host Determining latest release
+Write-Host "Finding latest release"
 $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
 
 $download = "https://github.com/$repo/releases/download/$tag/$file"
@@ -63,25 +63,41 @@ $name = $file.Split(".")[0]
 $zip = "$name-$tag.zip"
 $dir = "$name-$tag"
 
-Write-Host Dowloading latest release
+Write-Host "Dowloading latest release"
 Invoke-WebRequest $download -Out $zip
 
-Write-Host Extracting release files
+Write-Host "Extracting release files"
 Expand-Archive $zip -Force
 
 # Copy mod files into Valheim folder
 xcopy $dir $valheim /s /y
 
-# Download valheim_plus.cfg and put into game folder
-Write-Host `n
-Write-Host Downloading valheim_plus.cfg to game directory
-Invoke-WebRequest $vhp_config -Out $valheim/BepInEx/config/valheim_plus.cfg
-
 # Removing temp files
+Write-Host "Removing temp files"
 Remove-Item $zip -Force
 Remove-Item $dir -Recurse -Force
 
-Write-Host ""
+# Set up yes/no prompt
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Description."
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Description."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+# Ask if you'd like to update your local valheim_plus.cfg with the one used on the server to keep rulesets consistent
+$title = "Config download"
+$message = "Would you like to copy the server's valheim_plus.cfg file to your game install folder? This will allow you to use the same ruleset on your local worlds as we use on the server, but is not mandatory."
+$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+switch ($result) {
+  0{
+    # Download valheim_plus.cfg and put into game folder
+    Write-Host ""
+    Write-Host "Downloading valheim_plus.cfg to game directory"
+    Write-Host ""
+    Invoke-WebRequest $vhp_config -Out $valheim/BepInEx/config/valheim_plus.cfg
+  }1{
+    Write-Host ""
+  }
+}
+
 Write-Host ""
 Write-Host "Installation complete!"
 Write-Host ""
